2b90a43f858b0658ee31c46718b53508521c7e6b
Alexey Kosarev <ak555110689@gmail.com>
2024-04-03 15:51:23 +0600

    sadsasd

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -3,6 +3,15 @@ from .models import Product, ProductImage
 from ..categories.models import Category, SubCategory
 from ..categories.serializers import SubCategorySerializer
 
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели изображений продуктов."""
+
+    class Meta:
+        model = ProductImage
+        fields = ['image']
+
+
 class ProductImageSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True)
 
@@ -20,39 +29,15 @@ class ProductSerializer(serializers.ModelSerializer):
     sub_category = SubCategorySerializer()  # Сериализатор подкатегории включен для вложенности
     category = serializers.SerializerMethodField()  # Добавляем метод для получения категории
     images = ProductImageSerializer(many=True, read_only=True)
-    images_write = serializers.ListField(
-            child=serializers.ImageField(), required=False, write_only=True
-        )
     image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image', 'images', 'images_read', 'sub_category', 'category',
-                  'seller']
+        fields = ['id', 'name', 'description', 'price', 'image', 'images', 'sub_category', 'category', 'seller']
 
     def get_category(self, obj):
+        # Возвращаем сериализованные данные категории через объект подкатегории
         if obj.sub_category and obj.sub_category.category:
             return CategorySerializer(obj.sub_category.category).data
-        return None
-    def create(self, validated_data):
-        images_data = validated_data.pop('images', [])
-        product = Product.objects.create(**validated_data)
+        return None  # Или вернуть пустое значение, если категории нет
 
-        for image_data in images_data:
-            ProductImage.objects.create(product=product, image=image_data)
-
-        return product
-
-    def update(self, instance, validated_data):
-        images_data = validated_data.pop('images', None)
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-
-        if images_data is not None:
-            instance.images.all().delete()
-            for image_data in images_data:
-                ProductImage.objects.create(product=instance, image=image_data)
-
-        return instance
