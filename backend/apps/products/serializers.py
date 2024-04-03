a1d0c06ee2a11c0f9c07198b33a6b18f42601e52
Alexey Kosarev <ak555110689@gmail.com>
2024-04-03 15:45:36 +0600

    asdsad

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -19,16 +19,40 @@ class CategorySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     sub_category = SubCategorySerializer()  # Сериализатор подкатегории включен для вложенности
     category = serializers.SerializerMethodField()  # Добавляем метод для получения категории
-    images = ProductImageSerializer(many=True, read_only=True)
+    images_read = ProductImageSerializer(many=True, source='images', read_only=True)
+    images = serializers.ListField(
+            child=serializers.ImageField(), required=False, write_only=True
+        )
     image = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image', 'images', 'sub_category', 'category', 'seller']
+        fields = ['id', 'name', 'description', 'price', 'image', 'images', 'images_read', 'sub_category', 'category',
+                  'seller']
 
     def get_category(self, obj):
-        # Возвращаем сериализованные данные категории через объект подкатегории
         if obj.sub_category and obj.sub_category.category:
             return CategorySerializer(obj.sub_category.category).data
-        return None  # Или вернуть пустое значение, если категории нет
+        return None
+    def create(self, validated_data):
+        images_data = validated_data.pop('images', [])
+        product = Product.objects.create(**validated_data)
 
+        for image_data in images_data:
+            ProductImage.objects.create(product=product, image=image_data)
+
+        return product
+
+    def update(self, instance, validated_data):
+        images_data = validated_data.pop('images', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if images_data is not None:
+            instance.images.all().delete()
+            for image_data in images_data:
+                ProductImage.objects.create(product=instance, image=image_data)
+
+        return instance
