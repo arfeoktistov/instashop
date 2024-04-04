48c1fc48965ee7f0d67d66a2bdbd86bad861a5d4
Alexey Kosarev <ak555110689@gmail.com>
2024-04-04 15:27:13 +0600

    rework users and images

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -2,134 +2,93 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductCreateSerializer, ProductUpdateSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from .permissions import IsSellerProduct
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    permission_classes = (
+        IsAuthenticatedOrReadOnly,
+        IsSellerProduct,
+    )
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve', 'destroy']:
+            return ProductSerializer
+        elif self.action == 'create':
+            return ProductCreateSerializer
+        elif self.action == 'update':
+            return ProductUpdateSerializer
+        else:
+            pass
 
     @swagger_auto_schema(
         tags=['Продукты'],
         operation_summary="Получить список всех продуктов",
-        operation_description="Возвращает список всех продуктов с их изображениями и подробностями.",
         responses={
-            200: openapi.Response(
-                description="Список всех продуктов.",
-                schema=openapi.Schema(
-                    type=openapi.TYPE_ARRAY,
-                    items=openapi.Schema(
-                        type=openapi.TYPE_OBJECT,
-                        properties={
-                            "id": openapi.Schema(type=openapi.TYPE_INTEGER),
-                            "name": openapi.Schema(type=openapi.TYPE_STRING),
-                            "description": openapi.Schema(type=openapi.TYPE_STRING),
-                            "price": openapi.Schema(type=openapi.TYPE_STRING),
-                            "image": openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI),
-                            "images": openapi.Schema(
-                                type=openapi.TYPE_ARRAY,
-                                items=openapi.Items(
-                                    type=openapi.TYPE_OBJECT,
-                                    properties={
-                                        "image": openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_URI),
-                                    }
-                                ),
-                            ),
-                        },
-                    ),
-                ),
-            ),
-        },
+            200: ProductSerializer(many=True)
+        }
     )
-    def list(self, request: Request, *args, **kwargs) -> Response:
-        """Возвращает список всех продуктов."""
+    def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     @swagger_auto_schema(
         tags=['Продукты'],
+        operation_summary="Получить детали продукта",
+        responses={
+            200: ProductSerializer()
+        }
+    )
+    def retrieve(self, request, *pk, **kwargs):
+        return super().retrieve(request, *pk, **kwargs)
+
+    @swagger_auto_schema(
+        tags=['Продукты'],
         operation_summary="Создать новый продукт",
-        operation_description="Создает новый продукт с деталями и изображениями.",
-        request_body=ProductSerializer,
+        operation_description="Создает новый продукт с заданными деталями.",
+        request_body=ProductCreateSerializer,
         responses={
             201: openapi.Response(
                 description="Продукт успешно создан.",
-                schema=ProductSerializer,
+                schema=ProductCreateSerializer,
             ),
             400: "Некорректные данные запроса"
         }
     )
     def create(self, request, *args, **kwargs):
-        """Создает новый продукт."""
         return super().create(request, *args, **kwargs)
 
     @swagger_auto_schema(
         tags=['Продукты'],
-        operation_summary="Получить детали продукта по ID",
-        operation_description="Возвращает подробные сведения о продукте по его ID.",
+        operation_summary="Обновить продукт",
+        operation_description="Обновляет продукт с указанным ID.",
+        request_body=ProductUpdateSerializer,
         responses={
             200: openapi.Response(
-                description="Детали продукта",
-                schema=ProductSerializer,
-            ),
-            404: "Продукт не найден"
-        }
-    )
-    def retrieve(self, request, *args, **kwargs):
-        """Возвращает детали продукта по ID."""
-        return super().retrieve(request, *args, **kwargs)
-
-    @swagger_auto_schema(
-        tags=['Продукты'],
-        operation_summary="Обновить продукт по ID",
-        operation_description="Обновляет информацию о продукте с указанным ID.",
-        request_body=ProductSerializer,
-        responses={
-            200: openapi.Response(
-                description="Продукт обновлен.",
-                schema=ProductSerializer,
+                description="Продукт успешно обновлен.",
+                schema=ProductUpdateSerializer,
             ),
             400: "Некорректные данные запроса",
             404: "Продукт не найден"
         }
     )
     def update(self, request, *args, **kwargs):
-        """Обновляет продукт по ID."""
         return super().update(request, *args, **kwargs)
 
     @swagger_auto_schema(
         tags=['Продукты'],
-        operation_summary="Частично обновить продукт по ID",
-        operation_description="Обновляет часть данных продукта с указанным ID.",
-        request_body=ProductSerializer,
-        responses={
-            200: openapi.Response(
-                description="Продукт частично обновлен.",
-                schema=ProductSerializer,
-            ),
-            400: "Некорректные данные запроса",
-            404: "Продукт не найден"
-        }
-    )
-    def partial_update(self, request, *args, **kwargs):
-        """Частично обновляет продукт по ID."""
-        return super().partial_update(request, *args, **kwargs)
-
-    @swagger_auto_schema(
-        tags=['Продукты'],
-        operation_summary="Удалить продукт по ID",
-        operation_description="Удаляет продукт с указанным ID из системы.",
+        operation_summary="Удалить продукт",
+        operation_description="Удаляет продукт с указанным ID.",
         responses={
             204: "Продукт успешно удален",
             404: "Продукт не найден"
         }
     )
-    def destroy(self, request, *args, **kwargs):
-        """Удаляет продукт по ID."""
-        return super().destroy(request, *args, **kwargs)
+    def destroy(self, request, *pk, **kwargs):
+        return super().destroy(request, *pk, **kwargs)
 
 
 class SellerProductsViewSet(ReadOnlyModelViewSet):
@@ -140,17 +99,6 @@ class SellerProductsViewSet(ReadOnlyModelViewSet):
         seller_id = self.kwargs.get('seller_id')
         return Product.objects.filter(seller__id=seller_id) if seller_id else Product.objects.none()
 
-    @action(detail=True, methods=['get'], url_path='products_by_category')
-    @swagger_auto_schema(
-        tags=['Получение категорий по ID'],
-        operation_summary="Получить продукты продавца по категории и подкатегории",
-        operation_description="Возвращает продукты заданного продавца, фильтруя по категории и подкатегории.",
-        manual_parameters=[
-            openapi.Parameter('category_id', openapi.IN_QUERY, description="ID категории", type=openapi.TYPE_INTEGER, required=False),
-            openapi.Parameter('subcategory_id', openapi.IN_QUERY, description="ID подкатегории", type=openapi.TYPE_INTEGER, required=False),
-        ],
-        responses={200: ProductSerializer(many=True)}
-    )
     def products_by_category(self, request, *args, **kwargs):
         seller_id = self.kwargs.get('seller_id')
         category_id = request.query_params.get('category_id')
@@ -165,4 +113,3 @@ class SellerProductsViewSet(ReadOnlyModelViewSet):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-
--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,46 +1,32 @@
 from rest_framework.routers import DefaultRouter as DR
-
 from django.urls import path
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
-
 from apps.users.views import(
-    UserRegistrationView,
-    VerificationUserView,
-    NewVerificationCodeView,
-    UserViewSet, SellerUserViewSet,
-
-
+     SellerUserViewSet,
 )
 
 from apps.users.views import GetUserIdView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 router = DR()
-router.register(r'users', UserViewSet)
 router.register(r'seller-users', SellerUserViewSet)
+
+token_obtain_pair = TokenObtainPairView.as_view()
+token_obtain_pair = swagger_auto_schema(
+    method='post',
+    responses={200: openapi.Response('Токен получен',)},
+    tags=['Магазин'],
+    operation_summary="Авторизация. Получить пару токенов",
+)(token_obtain_pair)
 
 urlpatterns = [
     path(
         'token/',
-        TokenObtainPairView.as_view(), 
+        token_obtain_pair,
         name='token_obtain_pair'
-    ),
-    path(
-        'registration/', 
-        UserRegistrationView.as_view(), 
-        name='registration'
-    ),
-    path(
-        'verification/', 
-        VerificationUserView.as_view(), 
-        name='verification'
-    ),
-    path(
-        'new_code/', 
-        NewVerificationCodeView.as_view(), 
-        name='new_code'
     ),
     path('users/token/user-id/', GetUserIdView.as_view(), name='get-user-id'),
 
