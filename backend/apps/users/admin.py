089ae23c7c1e5dea08f1abc9102ee321dd640311
Alexey Kosarev <ak555110689@gmail.com>
2024-04-04 16:01:22 +0600

    users

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -7,8 +7,42 @@ from apps.users.models import(
 
 from django.contrib.auth.models import Group
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(SellerUser)
 
 admin.site.unregister(Group)
+from django.contrib.auth.admin import UserAdmin
+class UserAdminModel(UserAdmin):
+    list_display = (
+        'id','email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+        'is_superuser'
+    )
+    fieldsets = (
+        (
+            'Данные аккаунта', {
+                'fields': (
+                    'email',
+                    'password',
+                )
+            }
+        ),
+        (
+            'Персональная информация', {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'phone_number',
+                    'role',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        )
+    )
 
+admin.site.register(User, UserAdminModel)
--- backend/apps/users/signals.py
+++ backend/apps/users/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from apps.users.models import(
-    User, SellerUser
-)
-
-from apps.users.config import(
-    SELLER,
-)
-
-@receiver(post_save, sender=User)
-def create_connected_models(sender, instance, created, **kwargs):
-    if created:
-        if instance.role == SELLER:
-            SellerUser.objects.create(user=instance)
-
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+#
+# from apps.users.models import(
+#     User, SellerUser
+# )
+#
+# from apps.users.config import(
+#     SELLER,
+# )
+#
+# @receiver(post_save, sender=User)
+# def create_connected_models(sender, instance, created, **kwargs):
+#     if created:
+#         if instance.role == SELLER:
+#             SellerUser.objects.create(user=instance)
+#
