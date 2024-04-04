ce7be96f621d8034b9becd0653b8fb8bc2837da3
Alexey Kosarev <ak555110689@gmail.com>
2024-04-04 17:32:07 +0600

    sad

--- backend/apps/products/permissions.py
+++ backend/apps/products/permissions.py
@@ -5,4 +5,4 @@ class IsSellerProduct(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_authenticated and request.user.seller_user == obj.seller_user
+        return request.user.is_authenticated and request.user.seller_user == obj.seller
