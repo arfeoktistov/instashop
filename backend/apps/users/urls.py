2018b26bbe1ce4b0d4d153000541436157bb1ce6
Alexey Kosarev <ak555110689@gmail.com>
2024-04-04 15:42:27 +0600

    rework

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -6,7 +6,6 @@ from rest_framework_simplejwt.views import (
 from apps.users.views import(
      SellerUserViewSet,
 )
-
 from apps.users.views import GetUserIdView
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -14,18 +13,18 @@ from drf_yasg import openapi
 router = DR()
 router.register(r'seller-users', SellerUserViewSet)
 
-token_obtain_pair = TokenObtainPairView.as_view()
-token_obtain_pair = swagger_auto_schema(
-    method='post',
-    responses={200: openapi.Response('Токен получен',)},
-    tags=['Магазин'],
-    operation_summary="Авторизация. Получить пару токенов",
-)(token_obtain_pair)
+# token_obtain_pair = TokenObtainPairView.as_view()
+# token_obtain_pair = swagger_auto_schema(
+#     method='post',
+#     responses={200: openapi.Response('Токен получен',)},
+#     tags=['Магазин'],
+#     operation_summary="Авторизация. Получить пару токенов",
+# )(token_obtain_pair)
 
 urlpatterns = [
     path(
         'token/',
-        token_obtain_pair,
+        TokenObtainPairView.as_view(),
         name='token_obtain_pair'
     ),
     path('users/token/user-id/', GetUserIdView.as_view(), name='get-user-id'),
